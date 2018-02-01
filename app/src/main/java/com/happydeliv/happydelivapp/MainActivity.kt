bc01bf035ae05b1c8495d5606cdc1a037c990b38
Ibnu Muzzakkir <personalict@gmail.com>
2018-02-01 21:44:40 +0700

    Initial Commit

---
+++ app/src/main/java/com/happydeliv/happydelivapp/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.happydeliv.happydelivapp
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
