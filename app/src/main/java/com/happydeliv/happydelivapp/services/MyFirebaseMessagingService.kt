b5fc3c09eb97a7454c548da963d2c4da30232783
Ibnu Muzzakkir <personalict@gmail.com>
2018-02-24 18:16:28 +0700

    Handling firebase pending intent

--- app/src/main/java/com/happydeliv/happydelivapp/services/MyFirebaseMessagingService.kt
+++ app/src/main/java/com/happydeliv/happydelivapp/services/MyFirebaseMessagingService.kt
@@ -3,7 +3,9 @@ package com.happydeliv.happydelivapp.services
 import android.annotation.SuppressLint
 import android.app.NotificationChannel
 import android.app.NotificationManager
+import android.app.PendingIntent
 import android.content.Context
+import android.content.Intent
 import android.media.RingtoneManager
 import android.os.Build
 import android.support.v4.app.NotificationCompat
@@ -11,6 +13,7 @@ import android.util.Log
 import com.google.firebase.messaging.FirebaseMessagingService
 import com.google.firebase.messaging.RemoteMessage
 import com.happydeliv.happydelivapp.R
+import com.happydeliv.happydelivapp.ui.activity.detailpackage.DetailPackageActivity
 
 
 /**
@@ -21,7 +24,7 @@ import com.happydeliv.happydelivapp.R
 class MyFirebaseMessagingService : FirebaseMessagingService() {
 
     private val TAG = "MyFirebaseMsgService"
-
+    private var mTrackId : String? = null
     /**
      * Called when message is received.
      *
@@ -29,32 +32,13 @@ class MyFirebaseMessagingService : FirebaseMessagingService() {
      */
     // [START receive_message]
     override fun onMessageReceived(remoteMessage: RemoteMessage) {
-        // [START_EXCLUDE]
-        // There are two types of messages data messages and notification messages. Data messages are handled
-        // here in onMessageReceived whether the app is in the foreground or background. Data messages are the type
-        // traditionally used with GCM. Notification messages are only received here in onMessageReceived when the app
-        // is in the foreground. When the app is in the background an automatically generated notification is displayed.
-        // When the user taps on the notification they are returned to the app. Messages containing both notification
-        // and data payloads are treated as notification messages. The Firebase console always sends notification
-        // messages. For more see: https://firebase.google.com/docs/cloud-messaging/concept-options
-        // [END_EXCLUDE]
-
-        // TODO(developer): Handle FCM messages here.
-        // Not getting messages here? See why this may be: https://goo.gl/39bRNJ
         Log.d(TAG, "From: " + remoteMessage.from!!)
 
         // Check if message contains a data payload.
         if (remoteMessage.data.isNotEmpty()) {
-            Log.d(TAG, "Message data payload: " + remoteMessage.data)
-
-            if (/* Check if data needs to be processed by long running job */ true) {
-                // For long-running tasks (10 seconds or more) use Firebase Job Dispatcher.
-                scheduleJob()
-            } else {
-                // Handle message within 10 seconds
-                handleNow()
+            if(remoteMessage.data["body"] != null){
+                mTrackId = remoteMessage.data["body"]
             }
-
         }
 
         // Check if message contains a notification payload.
@@ -62,31 +46,6 @@ class MyFirebaseMessagingService : FirebaseMessagingService() {
             Log.d(TAG, "Message Notification Body: " + remoteMessage.notification!!.body!!)
             sendNotification(remoteMessage.notification!!.body!!)
         }
-
-        // Also if you intend on generating your own notifications as a result of a received FCM
-        // message, here is where that should be initiated. See sendNotification method below.
-    }
-    // [END receive_message]
-
-    /**
-     * Schedule a job using FirebaseJobDispatcher.
-     */
-    private fun scheduleJob() {
-        // [START dispatch_job]
-//        val dispatcher = FirebaseJobDispatcher(GooglePlayDriver(this))
-//        val myJob = dispatcher.newJobBuilder()
-//                .setService(MyJobService::class.java)
-//                .setTag("my-job-tag")
-//                .build()
-//        dispatcher.schedule(myJob)
-        // [END dispatch_job]
-    }
-
-    /**
-     * Handle time allotted to BroadcastReceivers.
-     */
-    private fun handleNow() {
-        Log.d(TAG, "Short lived task is done.")
     }
 
     /**
@@ -96,31 +55,33 @@ class MyFirebaseMessagingService : FirebaseMessagingService() {
      */
     @SuppressLint("NewApi")
     private fun sendNotification(messageBody: String) {
-//        val intent = Intent(this, MainActivity::class.java)
-//        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP)
-//        val pendingIntent = PendingIntent.getActivity(this, 0 /* Request code */, intent,
-//                PendingIntent.FLAG_ONE_SHOT)
+        val intent = Intent(this, DetailPackageActivity::class.java)
+        intent.putExtra("data", mTrackId)
+        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP)
+        val pendingIntent = PendingIntent.getActivity(this, 0 /* Request code */, intent,
+                PendingIntent.FLAG_ONE_SHOT)
 
-//        val channelId = getString(R.string.default_notification_channel_id)
         val defaultSoundUri = RingtoneManager.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION)
-        val notificationBuilder = NotificationCompat.Builder(this, "Tes channel id")
+        val notificationBuilder = NotificationCompat.Builder(this, "Happy deliv channel")
                 .setSmallIcon(R.drawable.ic_add)
-                .setContentTitle("FCM Message")
+                .setContentTitle("Happy Delivery App")
                 .setContentText(messageBody)
                 .setAutoCancel(true)
                 .setSound(defaultSoundUri)
-//                .setContentIntent(pendingIntent)
+                .setContentIntent(pendingIntent)
 
         val notificationManager = getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
 
         // Since android Oreo notification channel is needed.
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
-            val channel = NotificationChannel("Tes channel id",
-                    "Channel human readable title",
+            val channel = NotificationChannel("Happy deliv channel",
+                    "Happy Delivery App",
                     NotificationManager.IMPORTANCE_DEFAULT)
             notificationManager.createNotificationChannel(channel)
         }
 
         notificationManager.notify(0 /* ID of notification */, notificationBuilder.build())
     }
+
+
 }
